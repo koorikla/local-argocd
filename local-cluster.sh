96a6745955689a9ba104651648214a5befc3a277
kaur kallas <kaurkallas@kaurs-MacBook-Pro.local>
2022-04-05 23:25:47 +0300

    Quick and dirty local argocd setup

---
+++ local-cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#echo "Cleaning up old kind k8s cluster"
+#kind delete clusters kind
+
+echo "Creating Kind local k8s cluster, if a kind cluster already exists the error is ok"
+kind create cluster --config local-kind-cluster-non-HA.yaml
+
+echo "Now since the local cluster is up i'd reccomend to start k9s around now to see what is being deployed, just run 'k9s' in a seperate terminal"
+echo "And press 0 to see all pods (if it opens in pod view by default :pod if not)"
+
+echo "Pushing Argo"
+helm upgrade --install argo-cd argo/argo-cd --version 4.5.0 -n argocd --create-namespace --wait
+
+echo "Pushing repo creds and app definition to ArgoCD"
+kubectl apply -f repo-and-app.yaml
+
+echo "Access argo via the kubectl port-forward command outputed by helm installation above"
